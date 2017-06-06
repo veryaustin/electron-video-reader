7dfb07d25536e13c5818a1bf2d4432cf520e4f1c
Austin Lauritsen <austin@veryaustin.com>
2017-06-06 15:10:32 -0500

    Wire up ICP & ffmpeg to get video length. Added comments as well

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
+// Set mainWindow
+let mainWindow;
+
+// On App startup
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  // Create a new BrowserWindow
+  mainWindow = new BrowserWindow({});
+
+  // Use the node __dirname to load the index.html file into mainWindow using loadURL()
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+// Inter-process communication listen for the even "video:submit" sent from the mainWindow process
+ipcMain.on('video:submit', (event, path) => {
+  // Use ffmpeg probe to get the metadata about the video file.
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    // Send the video metadata.format.duration (Video Duration) back to the mainWindow process using the "video:metadata event"
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+  });
 });
